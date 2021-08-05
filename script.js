979be23eea55632457c31dab6d6fd18f13bfad87
PeterWanjihia <peter.wanjihia@student.moringaschool.com>
2021-08-05 23:28:20 +0300

    trying to make some changes in js

--- script.js
+++ script.js
@@ -46,25 +46,43 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 }
 
 
-// import css and plugin
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 
-// Install BootstrapVue
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=by-chloe&location=boston";
 
-import axios from "axios";
-
-term:restaurant;
-// the search keyword, type is "string", here we use "restaurant"
-latitude:1.2921;
-longitude:-36.8219;
-// the centrol point of San Francisco, you can also use "location" to search by address, type is "number"
-radius:500;
-// set the search range, type is number, unit is meter from the location or (latitude,longitude)
-limit:40;
-// set how many search results you want, type is number
-
+$.ajax({
+   url: myurl,
+   headers: {
+    'Authorization':'Bearer z-qkakCZ3rS7b4eoI-femfdjNrynoaXCWDuzD49nB9ZB_T9-a1DD4_p3QY_N3ao8Vwf3vSiUX64BKUFGrTeD_15U61MnYLn_e8nOljRpehDFF5Q6x8gQQZBEItYLYXYx',
+},
+   method: 'GET',
+   dataType: 'json',
+   success: function(data){
+       // Grab the results from the API JSON return
+       var totalresults = data.total;
+       // If our results are greater than 0, continue
+       if (totalresults > 0){
+           // Display a header on the page with the number of results
+           $('#results').append('<h5>We discovered ' + totalresults + ' results!</h5>');
+           // Itirate through the JSON array of 'businesses' which was returned by the API
+           $.each(data.businesses, function(i, item) {
+               // Store each business's object in a variable
+               var id = item.id;
+               var alias = item.alias;
+               var phone = item.display_phone;
+               var image = item.image_url;
+               var name = item.name;
+               var rating = item.rating;
+               var reviewcount = item.review_count;
+               var address = item.location.address1;
+               var city = item.location.city;
+               var state = item.location.state;
+               var zipcode = item.location.zip_code;
+               // Append our result into our page
+               $('#results').append('<div id="' + id + '" style="margin-top:50px;margin-bottom:50px;"><img src="' + image + '" style="width:200px;height:150px;"><br>We found <b>' + name + '</b> (' + alias + ')<br>Business ID: ' + id + '<br> Located at: ' + address + ' ' + city + ', ' + state + ' ' + zipcode + '<br>The phone number for this business is: ' + phone + '<br>This business has a rating of ' + rating + ' with ' + reviewcount + ' reviews.</div>');
+         });
+       } else {
+           // If our results are 0; no businesses were returned by the JSON therefor we display on the page no results were found
+           $('#results').append('<h5>We discovered no results!</h5>');
+       }
+   }
+});    
